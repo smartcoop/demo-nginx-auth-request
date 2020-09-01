cd53a24ac74ff0a17e3c3b1a3f4136c107cd1880
AnthonyLammens-smart <anl@smart.coop>
2020-09-01 16:33:37 +0200

    add controller/view to login and validate Token

---
+++ AuthenticationServer/Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AuthenticationServer.Models;
+using AuthenticationServer.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthenticationServer.Controllers
+{
+    public class AuthController : Controller
+    {
+        private UserService _userService;
+        public AuthController(IConfiguration config)
+        {
+            _userService = new UserService(config);
+        }
+        public IActionResult Index()
+        {
+            var model = new LoginViewModel { };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var username = _userService.Login(model);
+                if (!string.IsNullOrEmpty(username))
+                {
+                    await CreateSessionCookie(username);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError("", "Invalid login attempt");
+            return View("Index", model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Auth");
+
+        }
+
+        private async Task CreateSessionCookie(string username)
+        {
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties();
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+        }
+    }
+}
---
+++ AuthenticationServer/Controllers/TokenController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationServer.Controllers
+{
+    [Route("api/token")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet("validate")]
+        public HttpResponseMessage Validate()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+        }
+    }
+}
