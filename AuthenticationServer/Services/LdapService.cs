5275d407b985129a1111ccae4f1cee956fe291b7
AnthonyLammens-smart <anl@smart.coop>
2020-09-01 16:34:30 +0200

    add service to login from ldap or in memory user

---
+++ AuthenticationServer/Services/LdapService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.DirectoryServices;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthenticationServer.Services
+{
+    public class LdapService
+    {
+        public string ActiveDirectoryPath;
+        public LdapService(IConfiguration configRoot)
+        {
+            ActiveDirectoryPath = configRoot["AppSettings:LdapUrl"];
+        }
+        public string Login(string trigram, string password)
+        {
+            string userName = null;
+            var entry = new DirectoryEntry(ActiveDirectoryPath, trigram, password);
+            var searcher = new DirectorySearcher(entry);
+            searcher.SearchScope = SearchScope.OneLevel;
+
+            try
+            {
+                // search for user
+                var result = searcher.FindOne();
+                if (result != null)
+                {
+                    // get user info
+                    entry = new DirectoryEntry(ActiveDirectoryPath, trigram, password);
+                    searcher = new DirectorySearcher(entry);
+                    searcher.Filter = (Convert.ToString("(SAMAccountName=") + trigram) + ")";
+                    searcher.PropertiesToLoad.Add("givenName");
+                    result = searcher.FindOne();
+                    if (result != null)
+                    {
+                        userName = result.GetDirectoryEntry().Properties["mailNickname"].Value?.ToString();
+                    }
+                }
+            } catch
+            {
+            }
+
+            return userName;
+        }
+    }
+}
---
+++ AuthenticationServer/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthenticationServer.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthenticationServer.Services
+{
+    public class UserService
+    {
+        private Dictionary<string, string> _inMemoryUsers;
+        private LdapService _ldapService;
+        public UserService(IConfiguration config)
+        {
+            _ldapService = new LdapService(config);
+            _inMemoryUsers = new Dictionary<string, string>();
+            _inMemoryUsers.Add("bob", "bob");
+        }
+        public string Login(LoginViewModel loginViewModel)
+        {
+            var username = _ldapService.Login(loginViewModel.Username, loginViewModel.Password);
+            if (string.IsNullOrEmpty(username))
+            {
+                username = _inMemoryUsers.FirstOrDefault(x => x.Key == loginViewModel.Username && x.Value == loginViewModel.Password).Key;
+            }
+
+            return username;
+        }
+    }
+}
