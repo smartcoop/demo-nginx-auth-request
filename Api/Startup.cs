83d342960143337b5f25aa2be546fd48e6eb6b9c
AnthonyLammens-smart <anl@smart.coop>
2020-09-02 14:18:08 +0200

    add dockerFile + docker-compose

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -31,6 +31,7 @@ namespace Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase("/api");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
--- AuthenticationServer/Startup.cs
+++ AuthenticationServer/Startup.cs
@@ -46,6 +46,7 @@ namespace AuthenticationServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase("/authenticationserver");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
